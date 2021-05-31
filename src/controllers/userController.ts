0650d77afb930b81f947a3e3b80d3fb0188be4a1
igordreher <igord.sparrenberger@gmail.com>
2021-05-31 19:17:54 -0300

    Add routes boiler-plate code

---
+++ src/controllers/userController.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from "express";
+
+
+export default {
+
+    list: async (req: Request, res: Response) => {
+
+    },
+
+    find: async (req: Request, res: Response) => {
+
+    },
+
+    create: async (req: Request, res: Response) => {
+
+    },
+
+    update: async (req: Request, res: Response) => {
+
+    },
+
+    delete: async (req: Request, res: Response) => {
+
+    },
+
+    deleteAll: async (req: Request, res: Response) => {
+
+    },
+};
---
+++ src/routes.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import userController from 'controllers/userController';
+
+const router = Router();
+
+router.get('/api/v1/users', userController.list);
+router.get('/api/v1/users/user_id', userController.find);
+
+router.post('/api/v1/users', userController.create);
+router.patch('/api/v1/users/user_id', userController.update);
+
+router.delete('/api/v1/users', userController.deleteAll);
+router.delete('/api/v1/users/user_id', userController.delete);
+
+
+export default router;
