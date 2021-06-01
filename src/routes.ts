303127a310fbe21866eb8ba1fce905878cfac02e
igordreher <igord.sparrenberger@gmail.com>
2021-06-01 18:25:12 -0300

    Fix routes params

--- src/routes.ts
+++ src/routes.ts
@@ -4,13 +4,13 @@ import userController from 'controllers/userController';
 const router = Router();
 
 router.get('/api/v1/users', userController.list);
-router.get('/api/v1/users/user_id', userController.find);
+router.get('/api/v1/users/:user_id', userController.find);
 
 router.post('/api/v1/users', userController.create);
-router.patch('/api/v1/users/user_id', userController.update);
+router.patch('/api/v1/users/:user_id', userController.update);
 
 router.delete('/api/v1/users', userController.deleteAll);
-router.delete('/api/v1/users/user_id', userController.delete);
+router.delete('/api/v1/users/:user_id', userController.delete);
 
 
 export default router;
