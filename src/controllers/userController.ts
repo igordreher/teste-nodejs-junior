2d3520ad8f3a501ad9bcf40c47da3c5e8cb151af
igordreher <igord.sparrenberger@gmail.com>
2021-06-01 18:27:01 -0300

    Add list & find routes

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -5,12 +5,23 @@ import * as yup from 'yup';
 
 export default {
 
-    list: async (req: Request, res: Response) => {
-
+    list: async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const users = await User.find();
+            res.status(200).json(users);
+        } catch (error) {
+            next(error);
+        }
     },
 
-    find: async (req: Request, res: Response) => {
-
+    find: async (req: Request, res: Response, next: NextFunction) => {
+        const { user_id } = req.params;
+        try {
+            const user = await User.findOne({ _id: user_id });
+            res.status(200).json(user);
+        } catch (error) {
+            next(error);
+        }
     },
 
     create: async (req: Request, res: Response, next: NextFunction) => {
@@ -23,6 +34,7 @@ export default {
         try {
             await schema.validate({ email, password });
             const user = await User.create({ email, password });
+            console.log(user);
             res.status(201).json(user);
         } catch (error) {
             next(error);
